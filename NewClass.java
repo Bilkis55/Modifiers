40622d423d4bfe8b56fe2cd6d047a1a97eb66d6f
Bilkis55 <133634469+Bilkis55@users.noreply.github.com>
2023-06-09 17:46:42 +0530

    Update NewClass.java

--- NewClass.java
+++ NewClass.java
@@ -1,13 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author admin
- */
+import java.util.Scanner;
 public class NewClass {
-    
+    public static void main(String[]args)
+    {
+        Scanner myobj=new Scanner(System.in);
+        System.out.println("Enter user name=");
+        
+        String userName=myonj.nextLine();
+        System.out.println("Username is : " + userName);
+    }
 }
