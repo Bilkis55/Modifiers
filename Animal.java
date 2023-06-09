5df8b6f67334b0fafb86787696834e1e8be859d4
Bilkis55 <133634469+Bilkis55@users.noreply.github.com>
2023-06-09 17:39:06 +0530

    Update Animal.java

--- Animal.java
+++ Animal.java
@@ -1,13 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Admin
- */
 public class Animal {
-    
+    static void mymethod(String fname)
+    {
+        System.out.println(fname + " Elephant");
+    }
+    public static void main(String[]args)
+    {
+        mymethod("dog");
+        mymethod("cat");
+        mymethod("horse");
+    }
 }
